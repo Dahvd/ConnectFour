0ae5625892b9656ddc3d5fabe15a32843f2cafe4
David Oligney <davidoligney@gmail.com>
2021-12-05 12:42:44 -0700

    winner checking done

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import Column from "./Column";
 
-export default function Board( props ) {
+export default function Board(  ) {
   const [ player, setPlayer ] = useState( 1 );
   const [ winner, setWinner ] = useState( [ false, 0 ] );
   const temp = new Array( 7 ).fill( null );
+  const row = new Array( 6 ).fill( 0 );
+  const tempBoardState = temp.map( () => row );
+
+
+  const [ boardState, setBoardState ] = useState( tempBoardState );
+
   const board = temp.map( ( column, index ) =>
     <Column winner={winner} setWinner={setWinner}
-      key={index} player={player} setPlayer={setPlayer}/> );
+      key={index} index={index} player={player} setPlayer={setPlayer}
+      boardState={boardState} setBoardState={setBoardState}/> );
   let winnerShow = null;
   if( winner[0] ) winnerShow = "Player " + winner[1] + " has won the game!";
   return (
     <>
-      <View>
+      <Text>
         {winnerShow}
-      </View>
+      </Text>
       <View style={styles.container}>
         {board}
       </View>
--- src/components/board/Column.js
+++ src/components/board/Column.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TouchableHighlight, Text, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 import Slot from "./Slot";
 
 
@@ -13,7 +13,7 @@ export default function Column( props ) {
   return (
     <>
       <TouchableOpacity
-        disabled={props.winner[0] ? true : false}
+        disabled={props.winner[0] ? true : false || column[0] != 0}
         onPress={ () => {
           for ( let i = column.length - 1; i >= 0; i-- ) {
             if ( column[i] === 0 ) {
@@ -22,14 +22,112 @@ export default function Column( props ) {
               columnVisible[i] =
                 <Slot player={column[i]} index={i} key={i}/>;
 
+              props.setPlayer( props.player === 1 ? 2 : 1 );
+              setColumn( [ ...column ] );
+              setColumnVisible( [ ...columnVisible ] );
+
+              props.boardState[props.index] = column;
+              props.setBoardState( props.boardState );
               break;
             }
           }
 
-          props.setPlayer( props.player === 1 ? 2 : 1 );
+          // vertical
+          for( let j = 0; j < 3; j++ ) {
+            if( column[5-j] != 0
+              && column[5-j] == column[5-j-1]
+              && column[5-j-1] == column[5-j-2]
+              && column[5-j-2] == column[5-j-3] ) {
+              props.setWinner( [ true, column[5-j] ] );
+              break;
+            }
+          }
 
-          setColumn( [ ...column ] );
-          setColumnVisible( [ ...columnVisible ] );
+          // horizontal
+          for( let j = 0; j < 4; j++ ) {
+            for( let k = 0; k < 5; k++ ) {
+              if( props.boardState[j][5-k] != 0 &&
+                props.boardState[j][5-k] == props.boardState[j+1][5-k] &&
+                props.boardState[j+1][5-k] == props.boardState[j+2][5-k] &&
+                props.boardState[j+2][5-k] == props.boardState[j+3][5-k] ) {
+                props.setWinner( [ true, props.boardState[j][5-k] ] );
+                break;
+              }
+            }
+          }
+
+
+
+          console.log( props.boardState );
+          //24 diagonals total
+
+          //12 left to right diagonals
+          for( let j = 0; j < 4; j++ ) {
+            for( let k = 2; k < 6; k++ ) {
+              if( props.boardState[j][k] != 0 &&
+                props.boardState[j][k] == props.boardState[j+1][k+1] &&
+                props.boardState[j][k] == props.boardState[j+2][k+2] &&
+                props.boardState[j][k] == props.boardState[j+3][k+3] ) {
+                props.setWinner( [ true, props.boardState[j][k] ] );
+              }
+            }
+          }
+
+          for( let j = 0; j < 4; j++ ) {
+            for( let k = 1; k < 5; k++ ) {
+              if( props.boardState[j][k] != 0 &&
+                props.boardState[j][k] == props.boardState[j+1][k+1] &&
+                props.boardState[j][k] == props.boardState[j+2][k+2] &&
+                props.boardState[j][k] == props.boardState[j+3][k+3] ) {
+                props.setWinner( [ true, props.boardState[j][k] ] );
+              }
+            }
+          }
+
+          for( let j = 0; j < 4; j++ ) {
+            for( let k = 0; k < 4; k++ ) {
+              if( props.boardState[j][k] != 0 &&
+                props.boardState[j][k] == props.boardState[j+1][k+1] &&
+                props.boardState[j][k] == props.boardState[j+2][k+2] &&
+                props.boardState[j][k] == props.boardState[j+3][k+3] ) {
+                props.setWinner( [ true, props.boardState[j][k] ] );
+              }
+            }
+          }
+
+          //12 right to left diagons
+          for( let j = 6; j > 2; j-- ) {
+            for( let k = 2; k < 6; k++ ) {
+              if( props.boardState[j][k] != 0 &&
+                props.boardState[j][k] == props.boardState[j-1][k+1] &&
+                props.boardState[j][k] == props.boardState[j-2][k+2] &&
+                props.boardState[j][k] == props.boardState[j-3][k+3] ) {
+                props.setWinner( [ true, props.boardState[j][k] ] );
+              }
+            }
+          }
+
+          for( let j = 6; j > 2; j-- ) {
+            for( let k = 1; k < 5; k++ ) {
+              if( props.boardState[j][k] != 0 &&
+                props.boardState[j][k] == props.boardState[j-1][k+1] &&
+                props.boardState[j][k] == props.boardState[j-2][k+2] &&
+                props.boardState[j][k] == props.boardState[j-3][k+3] ) {
+                props.setWinner( [ true, props.boardState[j][k] ] );
+              }
+            }
+          }
+
+          for( let j = 6; j > 2; j-- ) {
+            for( let k = 0; k < 4; k++ ) {
+              if( props.boardState[j][k] != 0 &&
+                props.boardState[j][k] == props.boardState[j-1][k+1] &&
+                props.boardState[j][k] == props.boardState[j-2][k+2] &&
+                props.boardState[j][k] == props.boardState[j-3][k+3] ) {
+                props.setWinner( [ true, props.boardState[j][k] ] );
+              }
+            }
+          }
         }}
       >
         <>
